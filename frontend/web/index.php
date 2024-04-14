9f13a76eaf0703642ec6e1fabd85bd2c44569316
Marcel Hernandez <e6990620@gmail.com>
2024-04-15 00:05:59 +0200

    happy path, no user feedback

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -2,17 +2,49 @@
 
 declare(strict_types=1);
 
+use Nyholm\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
-require __DIR__ . '/../vendor/autoload.php';
+define('__ROOT__', dirname(__DIR__));
+
+require __ROOT__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) {
-    $response->getBody()->write("Hello, ke ase");
+$twig = new Environment(
+    new FilesystemLoader(__ROOT__.'/views'),
+    [
+        'debug' => true,
+        'strict_variables' => true,
+    ]
+);
+
+$app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) use ($twig) {
+    $response->getBody()->write($twig->render('index.html.twig'));
+
     return $response;
+});
+
+$app->post('/tx', function (ServerRequestInterface $request, ResponseInterface $response) use ($twig) {
+    ['tx' => $tx] = $request->getParsedBody();
+
+    if ($tx === '' || !preg_match('/^([0-9a-fA-F]{2})+$/', $tx)) {
+        return new Response(400, ['Content-Type' => 'text/plain'], 'Fuck off, mate');
+    }
+
+    $fh = fsockopen('localhost', 8787);
+    if ($fh === false) {
+        return new Response(500, ['Content-Type' => 'text/plain'], 'Cannot connect to GroupHug server');
+    }
+
+    fwrite($fh, "add_tx $tx");
+    fclose($fh);
+
+    return new Response(302, ['Location' => '/']);
 });
 
 $app->run();
